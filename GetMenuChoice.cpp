37467c4cb41da5068ef6cf87d76adf3b3315c864
pipa <vladik.zhizha.1999@mail.ru>
2026-05-22 15:08:58 +0300

    Добавлен GetMenuChoice.cpp

---
+++ GetMenuChoice.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int GetMenuChoice() {
+    int choice;
+    cout << "\nВыберите вариант:\n";
+    cout << "1 - разница кодов ASCII для латинской буквы\n";
+    cout << "2 - разница кодов Windows-1251 для русской буквы\n";
+    cout << "3 - вывод кода введенной цифры\n";
+    cout << "4 - выход\n";
+    cout << "Ваш выбор: ";
+    cin >> choice;
+    return choice;
+}
+
